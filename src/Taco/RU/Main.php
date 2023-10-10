a0139821a9ccf2f601a0be1492f4825cff516dd4
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-10 03:16:34 -0700

    Update Main.php

--- src/Taco/RU/Main.php
+++ src/Taco/RU/Main.php
@@ -1,74 +1,75 @@
-<?php 
+<?php
 
 namespace Taco\RU;
 
-use pocketmine\{event\Listener,
-	event\player\PlayerJoinEvent,
-	event\player\PlayerPreLoginEvent,
-	plugin\Plugin,
-	plugin\PluginBase,
-	utils\Config};
+use cooldogedev\BedrockEconomy\BedrockEconomy;
+use pocketmine\{event\Listener, event\player\PlayerJoinEvent, event\player\PlayerPreLoginEvent, plugin\Plugin, plugin\PluginBase, utils\Config};
 use Taco\RU\commands\RankUp;
 
-class Main extends PluginBase implements Listener  {
+class Main extends PluginBase implements Listener {
 
-	/**
-	 * @var array
-	 */
-	public $config;
+    /**
+     * @var array
+     */
+    public $config;
 
-	/**
-	 * @var Config
-	 */
-	public $ranks;
+    /**
+     * @var Config
+     */
+    public $ranks;
 
-	/**
-	 * @var self
-	 */
-	protected static $instance;
+    /**
+     * @var self
+     */
+    protected static $instance;
 
-	/**
-	 * @var Plugin $economy
-	 */
-	public $economy;
+    /**
+     * @var BedrockEconomy $economy
+     */
+    public $economy;
 
-	public function onEnable() : void {
-		self::$instance = $this;
-		@mkdir($this->getDataFolder());
-		$this->saveResource("config.yml");
-		$this->config = $this->getConfig()->getAll();
-		$this->ranks = new Config($this->getDataFolder() . "ranks.yml", Config::YAML);
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		if ($this->getServer()->getPluginManager()->getPlugin("EconomyAPI")) {
-			$this->economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-		} else throw new \Exception("You must have EconomyAPI installed to use this plugin! You can download this plugin at https://poggit.pmmp.io/p/EconomyAPI");
-		$this->getServer()->getCommandMap()->register("RankUp", new RankUp($this));
-	}
+    public function onEnable(): void {
+        self::$instance = $this;
+        @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $this->config = $this->getConfig()->getAll();
+        $this->ranks = new Config($this->getDataFolder() . "ranks.yml", Config::YAML);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-	/**
-	 * @return self
-	 */
-	public static function getInstance() : self {
-		return self::$instance;
-	}
+        // Initialize Bedrock Economy
+        $bedrockEconomy = $this->getServer()->getPluginManager()->getPlugin("BedrockEconomy");
+        if ($bedrockEconomy instanceof BedrockEconomy) {
+            $this->economy = $bedrockEconomy;
+        } else {
+            $this->getLogger()->error("You must have Bedrock Economy by CoolDoge installed to use this plugin!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
 
-	/**
-	 * @return API
-	 */
-	public function getAPI() : API {
-		return new API();
-	}
+        $this->getServer()->getCommandMap()->register("RankUp", new RankUp($this));
+    }
 
-	/**
-	 * @param PlayerPreLoginEvent $event
-	 */
-	public function preJoin(PlayerPreLoginEvent $event) : void {
-		$player = $event->getPlayer();
-		if (!$this->ranks->exists($player->getName())) {
-			$this->ranks->set($player->getName(), $this->config["default-rank"]);
-			$this->ranks->save();
-		}
-	}
+    /**
+     * @return self
+     */
+    public static function getInstance(): self {
+        return self::$instance;
+    }
 
+    /**
+     * @return API
+     */
+    public function getAPI(): API {
+        return new API();
+    }
 
+    /**
+     * @param PlayerPreLoginEvent $event
+     */
+    public function preJoin(PlayerPreLoginEvent $event): void {
+        $player = $event->getPlayer();
+        if (!$this->ranks->exists($player->getName())) {
+            $this->ranks->set($player->getName(), $this->config["default-rank"]);
+            $this->ranks->save();
+        }
+    }
 }
